6a53aba1d228b7f9062a274bac95ddb027ecee0b
Patrick Galdino <patrick@galdino.me>
2021-01-04 12:01:47 -0300

    add cron example and send message to discord server

--- app/app.js
+++ app/app.js
@@ -3,6 +3,7 @@ require('dotenv').config({
 });
 
 const discord = require('discord.js');
+const cron = require('node-cron');
 const moment = require('moment');
 
 
@@ -13,4 +14,21 @@ bot.login(ENV.BOT_ID);
 
 bot.on('ready', function () {
     console.log(`Bot ${bot.user.username} foi iniciado em ${moment().format('DD/MM/YYYY HH:mm:ss')}`);
+});
+
+bot.on('message', async msg => {
+    if(msg.author.bot || msg.channel.id != ENV.VALERIA_CHANNEL) return;
+
+    console.log(msg.content);
+});
+
+//minute hour day/month month day/week
+
+cron.schedule('* * * * *', async () => {
+    let dateSend = moment().format('DD/MM/YYYY HH:mm:ss');
+    let outputBot = `⏰  ${dateSend}  ⏰`;
+
+    bot.channels.cache.get(ENV.GENERAL_CHANNEL).send(outputBot);
+    
+    console.log(`Mensagem enviada em ${dateSend}`);
 });
